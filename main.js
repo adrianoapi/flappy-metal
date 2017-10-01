bc29edf505cc4624701e23d0c8a4197b3c49d834
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-01 14:49:41 -0300

    add métodos update, jump e restartGame

--- main.js
+++ main.js
@@ -5,19 +5,29 @@ var mainState = {
     },
     create: function () {
         game.state.backgroundColor = '#71c5cf';
-        
+
         // 'seta' a física do sistema
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.bird = game.add.sprite(100, 245, 'bird');
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000;
-        
+
         // aplica a função de pular
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
     },
     update: function () {
-
+        if (this.bird.y < 0 || this.bird.y > 490) {
+            this.restartGame();
+        }
+    },
+    jump: function () {
+        // Vertical velocidade do passário
+        this.bird.body.velocity.y = -350;
+    },
+    restartGame: function () {
+        // Inicia o main quando for chamado
+        game.state.start('main');
     },
 };
 
