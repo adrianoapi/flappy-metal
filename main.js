86e1de46db1215bcbcc250f1b69c539af9f41fe7
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-01 14:40:55 -0300

    update método create para inserir física no bird

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var mainState = {
     preload: function () {
-
+        // carrega o sprite
+        game.load.image('bird', 'assets/bird.png');
     },
     create: function () {
-
+        game.state.backgroundColor = '#71c5cf';
+        
+        // 'seta' a física do sistema
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.bird = game.add.sprite(100, 245, 'bird');
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
+        
+        // aplica a função de pular
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
     },
     update: function () {
 
