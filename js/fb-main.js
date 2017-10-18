d84c13a159765a01c5bbe3cd8f1c99547ac05b53
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-17 23:36:31 -0200

    add função para armazenar cookie score

--- js/fb-main.js
+++ js/fb-main.js
@@ -55,3 +55,15 @@ $(document).ready(function () {
     showSplash();
 
 });
+
+// Função de captrua do cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0)
+            return c.substring(name.length, c.leng);
+    }
+    return false;
+}
