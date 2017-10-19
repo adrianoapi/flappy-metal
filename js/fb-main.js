baed97db28241fa9e12bef42eafd252fcee26c8e
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-18 22:53:18 -0200

    setando o cookie

--- js/fb-main.js
+++ js/fb-main.js
@@ -67,3 +67,11 @@ function getCookie(cname) {
     }
     return false;
 }
+// Setando cookie
+ function setCookie(cname,cvalue,exdays)
+   {
+      var d = new Date();
+      d.setTime(d.getTime()+(exdays*24*60*60*1000));
+      var expires = "expires="+d.toGMTString();
+      document.cookie = cname + "=" + cvalue + "; " + expires;
+   }
